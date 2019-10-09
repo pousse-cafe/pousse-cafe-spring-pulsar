dd7c8fa7f1fe7ec9a721d6be150692c153039828
Gerard Dethier <g.dethier@gmail.com>
2019-10-09 15:35:32 +0200

    Fix auto-deploy.

--- can_auto_deploy.sh
+++ can_auto_deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 VERSION=$(xmlstarlet sel -N "n=http://maven.apache.org/POM/4.0.0" -t -v "/n:project/n:version" pom.xml)
+
+if [[ "$VERSION" == "" ]]
+then
+    VERSION=$(xmlstarlet sel -N "n=http://maven.apache.org/POM/4.0.0" -t -v "/n:project/n:parent/n:version" pom.xml)
+fi
+
 if [[ $VERSION == *-SNAPSHOT && "$TRAVIS_PULL_REQUEST" == false ]]
 then
 	exit 0
